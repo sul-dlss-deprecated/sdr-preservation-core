a97b11f757f3b5bad674f9a5b8de39416ad7b26e
Bess Sadler <bess@ibiblio.org>
2010-04-26 16:46:01 -0700

    main method has to come after the module definition

--- robots/deposit/transfer_object.rb
+++ robots/deposit/transfer_object.rb
@@ -4,13 +4,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../boot')
 
 require 'lyber_core'
 
-# This is the equivalent of a java main method
-if __FILE__ == $0
-  dm_robot = Deposit::TransferObject.new(
-          'deposit', 'transfer-object')
-  dm_robot.start
-end
-
 module Deposit
 
 # putting this code in a class method makes it easier to test
@@ -23,4 +16,12 @@ module Deposit
       FileUtilities.transfer_object(druid, DOR_WORKSPACE_DIR, SDR_DEPOSIT_DIR)
     end
   end
+end
+
+
+# This is the equivalent of a java main method
+if __FILE__ == $0
+  dm_robot = Deposit::TransferObject.new(
+          'deposit', 'transfer-object')
+  dm_robot.start
 end
