01455f12b1aaf9cfed6103177c982e864e37751b
Bess Sadler <bess@ibiblio.org>
2010-04-29 17:49:12 -0700

    Adding ability to load a fedora object with a given druid

--- robots/deposit/populate_metadata.rb
+++ robots/deposit/populate_metadata.rb
@@ -19,12 +19,22 @@ module Deposit
 # - notifies DOR of failure by: <i><b>need to be filled in</b></i>
 
   class PopulateMetadata < LyberCore::Robot
+    
+    attr_reader :obj
 
     # Override the robot LyberCore::Robot.process_item method.
     # * Makes use of the Robot Framework FileUtilities.
     def process_item(work_item)
       # Identifiers
       @druid = work_item.druid
+      @obj = self.get_fedora_object
+    end
+    
+    # fetch the fedora object from the repository so we can attach datastreams to it
+    # throw an error if we can't find the object
+    def get_fedora_object
+      Fedora::Repository.register(SEDORA_URI)
+      return ActiveFedora::Base.load_instance(@druid)
     end
     
     # once you know the druid, go find a bagit object corresponding to that druid id
@@ -32,6 +42,8 @@ module Deposit
     def fetch_bag
       
     end
+    
+    
     
   end
 end
--- spec/deposit/populate_metadata_spec.rb
+++ spec/deposit/populate_metadata_spec.rb
@@ -5,29 +5,43 @@ require 'deposit/populate_metadata'
 
 describe Deposit::PopulateMetadata do
 
+  context "processing a workitem" do
   before(:all) do
     # in the test environment, and only when we want to test against the SDR2_EXAMPLE_OBJECTS,
     # have these tests assume that the SDR2_EXAMPLE_OBJECTS dir is the SDR_DEPOSIT_DIR
     SDR_DEPOSIT_DIR = SDR2_EXAMPLE_OBJECTS
-  end
-  
-  before(:each) do
     @robot = Deposit::PopulateMetadata.new("deposit","populate-metadata")
     @mock_workitem = mock("workitem")
     
     # return druid:123 when work_item.druid is called
     @mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
   end
+    
+    it "should accept a workitem" do
+      @robot.process_item(@mock_workitem)
+    end
+    
+    it "should be able to access a fedora object" do
+      @robot.should respond_to("obj")
+    end
+    
+    it "should load a sedora object with the given druid" do
+      @robot.obj.should be_instance_of(ActiveFedora::Base)
+      @robot.obj.pid.should eql(@mock_workitem.druid)
+    end
+    
+    it "should throw an error if it can't find a sedora object with the given druid" do
+      pending("If we query sedora with a druid and don't get anything back, what's our fail behavior?")
+    end
   
-  it "should accept a workitem" do
-    @robot.process_item(@mock_workitem)
-  end
-  
-  it "should be able to find a bag corresponding to the workitem's druid" do
-    @robot.process_item(@mock_workitem)
-    bagit_filename = SDR_DEPOSIT_DIR + '/' + @mock_workitem.druid.split(":")[1]
-    bagit_filename.should eql("/usr/local/projects/sdr2/config/environments/../../sdr2_example_objects/jc837rq9922")
-    (File.directory? bagit_filename).should eql(true)
+    it "should be able to find a bag corresponding to the workitem's druid" do
+      bagit_filename = SDR_DEPOSIT_DIR + '/' + @mock_workitem.druid.split(":")[1]
+      bagit_filename.should eql("/usr/local/projects/sdr2/config/environments/../../sdr2_example_objects/jc837rq9922")
+      (File.directory? bagit_filename).should eql(true)
+    end
+    
+    
+    
   end
 
  #  it "should transfer an object" do
