7c8ea37dd4d7697c7ce05d5a8fc242ae5e479a61
Bess Sadler <bess@ibiblio.org>
2010-05-11 16:45:23 -0700

    Re-organizing my examples to split them into contexts

--- spec/deposit/populate_metadata_spec.rb
+++ spec/deposit/populate_metadata_spec.rb
@@ -4,20 +4,54 @@ require 'lyber_core'
 require 'deposit/populate_metadata'
 
 describe Deposit::PopulateMetadata do
+  
+  context "dealing with bagit objects" do
+    before(:each) do
+      @robot = Deposit::PopulateMetadata.new("deposit","populate-metadata")    
+      mock_workitem = mock("populate_metadata_workitem")
+      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
+    end
+    it "should be able to access a bag object" do
+      @robot.should respond_to(:bag)
+    end
+    
+    it "should know where to look for the bag object" do
+      @robot.should respond_to(:bag_directory)
+    end
+    
+    it "should look in SDR_DEPOSIT_DIR by default" do
+      @robot.bag_directory.should eql(SDR_DEPOSIT_DIR)
+    end
+    
+    it "should allow us to change the location of bag_directory" do
+      @robot.bag_directory = SDR2_EXAMPLE_OBJECTS
+      @robot.bag_directory.should eql(SDR2_EXAMPLE_OBJECTS)
+    end
+    
+    it "should be able to find a bag corresponding to the workitem's druid" do
+      mock_workitem = mock("populate_metadata_workitem")
+      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
+      @robot.bag_directory = SDR2_EXAMPLE_OBJECTS
+      @robot.process_item(mock_workitem)
+      @robot.bag_exists?.should eql(true)
+      (File.directory? @robot.bag).should eql(true)
+    end
+  end
+
 
   context "processing a workitem" do
-    before(:all) do
-      # This is needed by hudson. I don't know why it isn't being picked up from the 
-      # environment file. --bess
-      SOLR_URL = 'http://127.0.0.1:8983/solr/test'
+    before(:each) do
       
-      # in the test environment, and only when we want to test against the SDR2_EXAMPLE_OBJECTS,
-      # have these tests assume that the SDR2_EXAMPLE_OBJECTS dir is the SDR_DEPOSIT_DIR
       @robot = Deposit::PopulateMetadata.new("deposit","populate-metadata")    
+      @robot.bag_directory = SDR2_EXAMPLE_OBJECTS
+      
       mock_workitem = mock("populate_metadata_workitem")
       # return druid:jc837rq9922 when work_item.druid is called
       mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
       
+      # This is needed by hudson. I don't know why it isn't being picked up from the 
+      # environment file. --bess
+      SOLR_URL = 'http://127.0.0.1:8983/solr/test'
       Fedora::Repository.register(SEDORA_URI)
       ActiveFedora::SolrService.register(SOLR_URL)
       
@@ -33,38 +67,22 @@ describe Deposit::PopulateMetadata do
         obj = ActiveFedora::Base.new(:pid => mock_workitem.druid)
         obj.save
       rescue
+        $stderr.print $!
       end
     end
+    
     
     it "should be able to access a fedora object" do
       @robot.should respond_to(:obj)
     end
     
-    it "should be able to access a bag object" do
-      @robot.should respond_to(:bag)
-    end
-    
     it "should have a process_item method" do
       @robot.should respond_to(:process_item)
-    end
-    
-    it "should know where to look for the bag object" do
-      @robot.should respond_to(:bag_directory)
-    end
-    
-    it "should look in SDR_DEPOSIT_DIR by default" do
-      @robot.bag_directory.should eql(SDR_DEPOSIT_DIR)
-    end
-    
-    it "should allow us to change the location of bag_directory" do
-      @robot.bag_directory = SDR2_EXAMPLE_OBJECTS
-      @robot.bag_directory.should eql(SDR2_EXAMPLE_OBJECTS)
     end
           
     it "should accept a workitem passed to process_item" do
       mock_workitem = mock("populate_metadata_workitem")
       mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
-      @robot.bag_directory = SDR2_EXAMPLE_OBJECTS
       @robot.process_item(mock_workitem)
     end
     
@@ -77,14 +95,7 @@ describe Deposit::PopulateMetadata do
       @robot.obj.pid.should eql(mock_workitem.druid)
     end
     
-    it "should be able to find a bag corresponding to the workitem's druid" do
-      mock_workitem = mock("populate_metadata_workitem")
-      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
-      @robot.bag_directory = SDR2_EXAMPLE_OBJECTS
-      @robot.process_item(mock_workitem)
-      @robot.bag_exists?.should eql(true)
-      (File.directory? @robot.bag).should eql(true)
-    end
+
     
     #   
     # it "should throw an error if it can't find a sedora object with the given druid" do
