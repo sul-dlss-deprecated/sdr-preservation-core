330b019e0bcbbdf823c593e5bd000b522c6a4474
Bess Sadler <bess@ibiblio.org>
2010-05-11 13:36:01 -0700

    removing leftover code

--- spec/deposit/populate_metadata_spec.rb
+++ spec/deposit/populate_metadata_spec.rb
@@ -97,20 +97,4 @@ describe Deposit::PopulateMetadata do
     
   end
 
- #  it "should transfer an object" do
- # 
- # # create new transferObject
- #     transfer_robot = Deposit::TransferObject.new( "deposit", "transfer-object")
- # # mock out a workitem
- #     mock_workitem = mock("workitem")
- # # return druid:123 when work_item.druid is called
- #     mock_workitem.stub!(:druid).and_return("druid:123")
- # # verify that FileUtilies.transfer_obejct is called
- #     FileUtilities.should_receive(:transfer_object)
- # 
- # # actually call the function we are testing
- #     transfer_robot.process_item(mock_workitem)
- # 
- #   end
-
 end
