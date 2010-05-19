bbd88404cbce829340db04d35806636171488dfe
Xinlei Qiu <xinlei@dn0a207307.sunet>
2010-05-19 11:32:59 -0700

    Modified transfer_obj_spec to use new module name and merge with tests made locally

--- spec/sdrIngest/transfer_object_spec.rb
+++ spec/sdrIngest/transfer_object_spec.rb
@@ -21,7 +21,7 @@ describe SdrIngest::TransferObject do
       end
       
       it "should transfer an object locally" do
-
+        pending()
         # create new transferObject
         transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
         # mock out a workitem
@@ -35,23 +35,23 @@ describe SdrIngest::TransferObject do
         transfer_robot.process_item(mock_workitem)
       end
       
-      # it "should find the transferred object in the local destination" do
-      #     
-      #   # create new transferObject
-      #   transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
-      #   # mock out a workitem
-      #   mock_workitem = mock("workitem")
-      #   
-      #   objId = "jc837rq9922"        
-      #   if !File.exist?(DOR_WORKSPACE_DIR + objId) then
-      #     raise('You need to get the test obj first.  Do "git submodule init", then "git submodule update".')
-      #   end
-      #    
-      #   # return druid:123 when work_item.druid is called
-      #   mock_workitem.stub!(:druid).and_return(objId)
-      #   # actually call the function we are testing
-      #   transfer_robot.process_item(mock_workitem).should == true
-      # end
+      it "should find the transferred object in the local destination" do
+        pending()
+        # create new transferObject
+        transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
+        # mock out a workitem
+        mock_workitem = mock("workitem")
+        
+        objId = "jc837rq9922"        
+        if !File.exist?(DOR_WORKSPACE_DIR + objId) then
+          raise('You need to get the test obj first.  Do "git submodule init", then "git submodule update".')
+        end
+         
+        # return druid:123 when work_item.druid is called
+        mock_workitem.stub!(:druid).and_return(objId)
+        # actually call the function we are testing
+        transfer_robot.process_item(mock_workitem).should == true
+      end
     end
     
     context "network transfers" do
@@ -79,14 +79,33 @@ describe SdrIngest::TransferObject do
          pending()
       end
 
-      it "should report error when network transfer failed" do
-         pending()
-       end
     end
 
   end 
   
   context "error conditions" do
+    it "should report error when source server doesn't exist" do
+      pending()
+      DOR_WORKSPACE_DIR="blaryehryh.stanford.edu:/tmp/dorWorkspaceDir"
+      SDR_DEPOSIT_DIR="/tmp/sdrDepositDir"
+        
+      # create new transferObject
+      transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
+      # mock out a workitem
+      mock_workitem = mock("workitem")
+      # return foo when work_item.druid is called
+      mock_workitem.stub!(:druid).and_return("foo")
+      # verify that FileUtilies.transfer_obejct is called
+      FileUtilities.should_receive(:transfer_object).with("foo", DOR_WORKSPACE_DIR, SDR_DEPOSIT_DIR).once
+
+      # actually call the function we are testing
+        transfer_robot.process_item(mock_workitem).should == false
+    end
+
+    it "should report error when destination server doesn't exist" do
+      pending()
+    end
+
     it "should report error when source is unreadable" do
       pending()
     end
@@ -94,17 +113,14 @@ describe SdrIngest::TransferObject do
     it "should report error when destination is unwritable" do
       pending()
     end
-
-    it "should report error when source server doesn't exist" do
-      pending()
-    end
-
-    it "should report error when destination server doesn't exist" do
+    
+    it "should check for pre-existing object of the same druid" do
       pending()
     end
     
     it "should report when transfer failed" do
-      pending()
+      # Existence of the destination file is checked by the FileUtilities.transfer_object call.
+      # Validation of the object is delayed to the validateBag robot.
     end
     
   end
