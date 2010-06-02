adce8056284bcbdce832dfe1c7d490181a02b9c9
Donald Lee <donlee@stanford.edu>
2010-06-01 18:29:01 -0700

    validate_bag_spec.rb now works

--- spec/sdrIngest/validate_bag_spec.rb
+++ spec/sdrIngest/validate_bag_spec.rb
@@ -7,74 +7,26 @@ describe SdrIngest::ValidateBag do
 
   context "validate" do
     
-      it "should return nil" do
+      it "should return nil when bag is valid" do
         robot = SdrIngest::ValidateBag.new("sdrIngest", "validate-bag")
         mock_workitem = mock("workitem")
         mock_workitem.stub!(:druid).and_return("druid:123")
         mock_bag = mock("bag")
-        mock_bag.stub!(:valid).and_return(true)
-        
-        BagIt::Bag.should_receive(:valid).once
+        BagIt::Bag.stub!(:new).and_return(mock_bag)
+        mock_bag.stub!(:valid?).and_return(true)
         
         robot.process_item(mock_workitem).should be_nil
       end
-      
-    #   it "should return true if it is a successful transfer" do
-    #     
-    #     # create new transferObject
-    #     transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
-    #     # mock out a workitem
-    #     mock_workitem = mock("workitem")
-    #     # return druid:123 when work_item.druid is called
-    #     mock_workitem.stub!(:druid).and_return("druid:123")
-    #     
-    #     File.stub!(:exists?).and_return(false)
-    #     FileUtilities.stub!(:transfer_object).and_return(true)
-    #     
-    #     # verify that FileUtilies.transfer_obejct is called
-    #     FileUtilities.should_receive(:transfer_object).with("druid:123", DOR_WORKSPACE_DIR, SDR_DEPOSIT_DIR).once
-    # 
-    #     # actually call the function we are testing
-    #     transfer_robot.process_item(mock_workitem).should == true
-    #   end
-    #   
-    #   it "should raise and error if transfer fails" do
-    #     # create new transferObject
-    #     transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
-    #     # mock out a workitem
-    #     mock_workitem = mock("workitem")
-    #     # return druid:123 when work_item.druid is called
-    #     mock_workitem.stub!(:druid).and_return("druid:123")
-    #     
-    #     File.stub!(:exists?).and_return(false)
-    #     FileUtilities.stub!(:transfer_object).and_raise("rsync failed")
-    #     
-    #     # verify that FileUtilies.transfer_obejct is called
-    #     FileUtilities.should_receive(:transfer_object).with("druid:123", DOR_WORKSPACE_DIR, SDR_DEPOSIT_DIR).once
-    # 
-    #     # actually call the function we are testing
-    #     lambda {transfer_robot.process_item(mock_workitem)}.should raise_error
-    #   end
-    # 
-    # it "should not transfer a pre-existing object of the same druid" do
-    #   # create new transferObject
-    #   transfer_robot = SdrIngest::TransferObject.new( "sdrIngest", "transfer-object")
-    #   # mock out a workitem
-    #   mock_workitem = mock("workitem")
-    # 
-    #   objId = "druid:123"
-    #   # return druid:123 when work_item.druid is called
-    #   mock_workitem.stub!(:druid).and_return(objId)
-    #   
-    #   File.stub!(:exists?).and_return(true)
-    # 
-    #   # verify that FileUtilies.transfer_obejct is never called
-    #   FileUtilities.should_receive(:transfer_object).never
-    # 
-    #   # actually call the function we are testing
-    #   lambda {transfer_robot.process_item(mock_workitem)}.should raise_error(/Object already exists/)
-    #   
-    # end
-            
+
+      it "should raise error when bag is not valid" do
+        robot = SdrIngest::ValidateBag.new("sdrIngest", "validate-bag")
+        mock_workitem = mock("workitem")
+        mock_workitem.stub!(:druid).and_return("druid:123")
+        mock_bag = mock("bag")
+        BagIt::Bag.stub!(:new).and_return(mock_bag)
+        mock_bag.stub!(:valid?).and_return(false)
+        
+        lambda {robot.process_item(mock_workitem)}.should raise_error
+      end
   end
 end
