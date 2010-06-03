6372120cf0d5b585ec1ba1ff87b6be5a2ff435ed
Bess Sadler <bess@ibiblio.org>
2010-06-03 13:13:05 -0700

    Progress.

--- features/step_definitions/workflow_steps.rb
+++ features/step_definitions/workflow_steps.rb
@@ -1,4 +1,12 @@
-WORKFLOW_SERVICE_URL = "http://lyberservices-dev.stanford.edu/workflow"
+require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+# require '../support/workflow_helpers.rb'
+
+WORKFLOW_SERVICE_URL = "http://lyberservices-dev.stanford.edu:8080/workflow"
+DOR_DEV_FEDORA_URL = "http://dor-dev.stanford.edu/fedora/"
+testpid ||= Nokogiri::XML(open(DOR_DEV_FEDORA_URL << "/management/getNextPID?xml=true&namespace=sdrtwo", {:http_basic_authentication=>["fedoraAdmin", "fedoraAdmin"]})).xpath("//pid").text
+
 
 # This is a dummy test. It just helps the cucumber read more easily. 
 When /^I want to test the sedora ingest workflow$/ do
@@ -6,14 +14,16 @@ When /^I want to test the sedora ingest workflow$/ do
 end
 
 
+# Can I get to the WORKFLOW_SERVICE_URL without raising an exception
 Then /^I should be able to talk to the workflow service$/ do
-  require 'net/http'
   lambda { Net::HTTP.get_response(URI.parse(WORKFLOW_SERVICE_URL))}.should_not raise_exception()
+  # TODO: Do I get a 200 response code?
 end
 
-When /^I create a new object$/ do
-  
-  ENABLE_SOLR_UPDATES = false
+# This is a re-write of the test_dor_object_creator script that Willy wrote
+Then /^I should be able to create a new object in DOR for testing against$/ do
+
+ ENABLE_SOLR_UPDATES = false
 
   require 'rubygems'
   require 'lyber_core'
@@ -28,22 +38,12 @@ When /^I create a new object$/ do
   </workflow>
   EOXML
 
-  if(ARGV.size != 1)
-    puts 'You must provide a pid'
-    exit
-  end
-
-  pid = ARGV[0]
-
   Fedora::Repository.register('http://fedoraAdmin:fedoraAdmin@dor-dev.stanford.edu/fedora')
-  obj = ActiveFedora::Base.new(:pid => pid, :label => 'sdr robot testing')
+  obj = ActiveFedora::Base.new(:pid => testpid, :label => 'sdr robot testing')
   obj.save
-
-  Dor::WorkflowService.create_workflow('dor', pid, 'googleScannedBookWF', xml)
-
-  puts 'Created dor object and workflow for ' << pid
-
-
-
-
+  Dor::WorkflowService.create_workflow('dor', testpid, 'googleScannedBookWF', xml)
+  
+  # our new PID should be in the sdrtwo namespace
+  obj.pid.should include("sdrtwo")
 end
+
