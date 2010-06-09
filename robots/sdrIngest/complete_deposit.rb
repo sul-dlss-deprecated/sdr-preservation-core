0947064fef5e94b754403e8aac6b060c9d3dd2b3
Xinlei Qiu <xinlei@dn0a20729e.sunet>
2010-06-09 14:38:55 -0700

    NOT WORKING, in process of restructuring everything.

--- robots/sdrIngest/complete_deposit.rb
+++ robots/sdrIngest/complete_deposit.rb
@@ -3,9 +3,15 @@
 # xinlei@stanford.edu
 # 13 May 2010
 
+$:.unshift File.join(File.dirname(__FILE__), "../../../../", "lybots/models/provenance_metadata")
+
 require File.expand_path(File.dirname(__FILE__) + '/../boot')
+require File.expand_path(File.dirname(__FILE__) + '/provenance_metadata')
+require File.expand_path(File.dirname(__FILE__) + '/workflow_model')
+#require File.expand_path(File.dirname(__FILE__) + '/sdr_service')
 
 require 'lyber_core'
+require 'nokogiri'
 
 # +Deposit+ initializes the SdrIngest workflow by registering the object and transferring 
 # the object from DOR to SDR's staging area.
@@ -17,10 +23,9 @@ require 'lyber_core'
 module SdrIngest
   
   class CompleteDeposit < LyberCore::Robot
-    attr_reader :obj, :druid;
+    attr_reader :obj, :druid, :sdr_provXML;
     attr_writer :bag_directory;
-    
-    
+        
     def initialize(string1,string2)
       super(string1,string2)
       # by default, get the bags from the SDR_DEPOSIT_DIR
@@ -30,16 +35,72 @@ module SdrIngest
     
     def process_item(work_item)
       @druid = work_item.druid
-      result = Dor::WorkflowService.update_workflow_status("sdr", druid, "sdrIngestWF", "complete-deposit", "completed")
-      raise "Update workflow \"complete-deposit\" failed" unless result
-      
-      raise "Cannot load Sedora object" unless get_fedora_object
-      
-      update_provenance(druid, "deposit complete")
+      raise "Cannot load Sedora object." unless get_fedora_object
+
+      # Update provenance
+      raise "Failed to update provenance to include Deposit completion." unless update_provenance
+
+      # Update DOR workflow 
+      result = Dor::WorkflowService.update_workflow_status("dor", druid, "googleScannedBookWF", "sdr-ingest-complete", "completed")
+      raise "Update workflow \"complete-deposit\" failed." unless result
     end
     
-    def update_provenance (druid, provenance)
+    def update_provenance 
+      # Create (and add?) new SDR prov datastream
+      create_sdr_provenance
+      
+      # retrieve existing prov
+      prov = retrieve_existing_provenance
+      #raise "Provenance metadata datastream not found." unless prov       
+
+      # append new prov to existing
+      append_provenance(prov, sdr_provXML)
+
+      # delete existing provenance datastream
+      # make sure to check the old one is indeed deleted.
+      
+      # Add prov back in
+      
+      ds_id = "PROVENANCE"
+      ds_label = "Provenance Metadata"
+#      self.obj.add_datastream(:pid=>@druid, prov.to_s) 
+#self.obj.add_datastream(:pid => @druid, :dsid=>ds_id, :dsLabel=>ds_label, :content=>prov.to_s) 
       return true
+    end
+ 
+    def append_provenance(prov_str, sdr_agent)
+      provXML = Nokogiri::XML(prov_str)
+      #provXML.root << sdr_provXML.root
+    end
+    
+    def retrieve_existing_provenance
+      ds_id = "PROVENANCE"
+      ds_label = "Provenance Metadata"
+      prov = self.obj.datastreams[ds_id]
+      
+    end
+    
+    def create_sdr_provenance
+      #workflowXML = SdrService.get_datastream(@druid, 'sdrIngestWF')
+      #workflowRoxml = WorkflowModel.from_xml(workflowXML)
+      #events = workflowRoxml.get_events()
+      event = Event.new
+      event.event = "SDR event"
+      event.who="SDR-robot"
+      event.when = "whenever"
+      
+      what = What.new
+      what.object = @druid
+#      what.event = events 
+      what.event = [event]
+      
+      agent = Agent.new
+      agent.name = "SDR"
+      agent.what = what
+      
+      @sdr_provXML = agent.to_xml
+# Why would this cause all the tests to fail?
+#      puts "SDR Agent: " + agent.to_xml
     end
     
     # fetch the fedora object from the repository so we can attach datastreams to it
@@ -58,8 +119,6 @@ module SdrIngest
   end
   
 end
-
-
 
 
 # This is the equivalent of a java main method
--- spec/sdrIngest/complete_deposit_spec.rb
+++ spec/sdrIngest/complete_deposit_spec.rb
@@ -1,4 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../robots/sdrIngest/provenance_metadata')
+
 require 'rubygems'
 require 'lyber_core'
 require 'sdrIngest/complete_deposit'
@@ -46,45 +48,16 @@ describe SdrIngest::CompleteDeposit do
     rescue
       $stderr.print $!
     end
-  
-  end
-  
-  context "Update workflow" do
-    before(:each) do
-      setup
-    end
-    
-    after(:each) do
-      cleanup
-    end
-    
-    it "should be able to process a work item" do
-      @complete_robot.should respond_to(:process_item)
-    end
-    
-    it "should update Sedora workflow to DepositComplete" do
-      mock_workitem = mock("complete_deposit_workitem")
-      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
 
-      # verify that Dor::WorkflowService.update_workflow_status is called      
-      Dor::WorkflowService.stub(:update_workflow_status).and_return(true)
-      Dor::WorkflowService.should_receive(:update_workflow_status).with("sdr", "druid:jc837rq9922", "sdrIngestWF", "complete-deposit", "completed")
-      
-      # actually call the function we are testing
-      @complete_robot.process_item(mock_workitem).should == true
-    end
-    
-    it "should report an error if workflow update failed" do
-      mock_workitem = mock("complete_deposit_workitem")
-      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
-      
-      Dor::WorkflowService.stub(:update_workflow_status).and_raise("Update workflow \"complete-deposit\" failed")
-      Dor::WorkflowService.should_receive(:update_workflow_status).with("sdr", "druid:jc837rq9922", "sdrIngestWF", "complete-deposit", "completed")
-      
-      lambda {@complete_robot.process_item(mock_workitem)}.should raise_error("Update workflow \"complete-deposit\" failed")
-    end
   end
   
+  context "basic behavior" do
+    it "should be able to process a work item" do
+      complete_robot = SdrIngest::CompleteDeposit.new("sdrIngest","complete-deposit")          
+      complete_robot.should respond_to(:process_item)
+    end
+  end
+    
   context "can load Sedora object with corresponding druid" do
     before(:each) do
       setup
@@ -124,38 +97,208 @@ describe SdrIngest::CompleteDeposit do
     after(:each) do
       cleanup
     end
+
+    it "should respond to update_provenance" do
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+
+      @complete_robt.stub!(:update_provenance).and_return(true)
+      @complete_robot.should_receive(:update_provenance).and_return(true)
+      @complete_robot.process_item(mock_workitem)
+    end
+
+    it "should raise error if update provenance fails" do
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+
+      @complete_robot.stub!(:update_provenance).and_return(false)
+      lambda {@complete_robot.process_item(mock_workitem)}.should raise_exception("Failed to update provenance to include Deposit completion.")
+    end
     
-    it "should update provenance to DepositComplete" do 
+    it "should respond positively to create_sdr_provenance" do
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+      
+      @complete_robot.should_receive(:create_sdr_provenance)
+      @complete_robot.process_item(mock_workitem)
+    end
+    
+    # Test to make sure create_sdr_provenance is creating valid SDR prov md 
+    it "should create valid new SDR provenance metadata" do 
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+
+      @complete_robot.should_receive(:create_sdr_provenance)
+      @complete_robot.process_item(mock_workitem)
+      @complete_robot.sdr_provXML.should_not be_nil
+      @complete_robot.sdr_provXML.should_not be_empty
+      puts @complete_robot.sdr_provXML.class
+#      @complete_robot.sdr_provXML.should be_instance_of()
+    end
+    
+    # This and the next test would not necessarily be true if SDR keeps its own prov datastream
+    it "should have a provenance datastream" do
       pending
       
       objectID = "druid:jc837rq9922"
       mock_workitem = mock("complete_deposit_workitem")
       mock_workitem.stub!(:druid).and_return(objectID)
 
-      #@complete_robot.process_item(mock_workitem)
-            
-      @complete_robot.stub!(:update_provenance).and_return(true)
-      @complete_robot.should_receive(:update_provenance).with("deposit complete")
-      
-      # Load the Sedora object and update its provenance datastream
+      # Load the Sedora object and retrieve its provenance datastream
+      ds = {"CONTENT" => "content", "PROVENANCE" => "provenance"}
       obj = ActiveFedora::Base.load_instance(mock_workitem.druid)
+      @complete_robot.stub!(:obj).and_return(obj)
       
-
+      obj.stub!(:datastreams).and_return(ds)
+      obj.should_receive(:datastreams).and_return({"PROVENANCE"=>"provenance"})
+      @complete_robot.process_item(mock_workitem)
     end
     
-    it "should report an error if update provenance failed" do
+    it "should raise an error if the obj doesn't have a provenance md datastream" do
+      pending
+      
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+      
+      ds = {"CONTENT" => "content"}
+      obj = ActiveFedora::Base.load_instance(mock_workitem.druid)
+      @complete_robot.stub!(:obj).and_return(obj)
+      
+      # The object has no PROVENANCE md ds
+      obj.stub!(:datastreams).and_return(ds)
+      obj.should_receive(:datastreams).and_return({"PROVENANCE"=>nil})
+      
+      lambda {@complete_robot.process_item(mock_workitem)}.should raise_exception("Provenance metadata datastream not found.")
+    end
+    
+    # how to test Nokogiri::XML(xml)???
+    it "should verify the existing provenance datastream contains valid XML" do
+      pending
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+      
+      ex_prov_xml = %{<?xml version="1.0"?>
+      <provenanceMetadata objectId="druid:jc837rq9922">
+          <agent name="DOR">
+          </agent>
+        </provenanceMetadata>
+      }
+      
+            event = Event.new
+            event.event = "SDR event"
+            event.who="SDR-robot"
+            event.when = "whenever"
+
+            what = What.new
+            what.object = @druid
+      #      what.event = events 
+            what.event = [event]
+
+            agent = Agent.new
+            agent.name = "SDR"
+            agent.what = what
+            
+      @complete_robot.stub!(:create_sdr_provenance).and_return(agent)
+      @complete_robot.stub!(:retrieve_existing_provenance).and_return(ex_prov_xml)
+
+      @complete_robot.should_receive(:append_provenance).with(ex_prov_xml, agent)
+#      Nokogiri::XML.should_receive(:parse).with(ex_prov_xml)
+
+      @complete_robot.process_item(mock_workitem)
+    end
+     
+    it "should append SDR provenance to existing provenance if there is any" do
+      pending
+      
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+      
+      ex_prov_xml = %{<?xml version="1.0"?>
+      <provenanceMetadata objectId="druid:jc837rq9922">
+          <agent name="DOR">
+          </agent>
+        </provenanceMetadata>
+      }
+      ex_prov = Nokogiri::XML(ex_prov_xml)
+      
+      sdr_prov_xml = %{   <agent name="SDR">
+          </agent>}
+      sdr_prov = Nokogiri::XML(sdr_prov_xml)
+      
+      @complete_robot.process_item(mock_workitem)
+  
+    end
+    
+    it "should delete existing provenance metadata datastream" do
+      pending
+        # Append SDR md to existing provenance md datastream
+        @complete_robot.stub!(:update_provenance).and_return(true)
+        @complete_robot.should_receive(:update_provenance).with("deposit complete")
+        SdrService.should_receive(:update_datastream).with(objectID, "PROVENANCE")
+    
+    end
+    
+    it "should raise error if deleting existing prov ds failed" do
       pending
     end
+    
+    it "should add new prov md ds" do
+      pending
+      
+      objectID = "druid:jc837rq9922"
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return(objectID)
+ 
+      @complete_robot.stub!(:create_sdr_provenance)
+      @complete_robot.stub!(:retrieve_existing_provenance).and_return("xml_str")
+      @complete_robot.stub!(:append_provenance)
+      
+      obj = ActiveFedora::Base.load_instance(mock_workitem.druid)
+
+      # The call looks like this:     self.obj.add_datastream(:pid=>@druid, :dsid=>ds_id, :dsLabel=>ds_label, :content=>provXML.to_s) 
+      # How to test the parameters?
+      obj.should_receive(:add_datastream)
+      @complete_robot.process_item(mock_workitem)
+    end
+    
   end
   
-  context "Notify DOR" do
-    it "should notify DOR of deposit complete" do
-      pending()
-    end
-  
-    it "should report an error if notifying DOR failed" do
-      pending
+  context "Update DOR workflow" do
+    before(:each) do
+      setup
     end
     
+    after(:each) do
+      cleanup
+    end
+        
+    it "should update DOR workflow to sdr-deposit-complete" do
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
+
+      # verify that Dor::WorkflowService.update_workflow_status is called      
+      Dor::WorkflowService.stub(:update_workflow_status).and_return(true)
+      Dor::WorkflowService.should_receive(:update_workflow_status).with("dor", "druid:jc837rq9922", "googleScannedBookWF", "sdr-ingest-complete", "completed")
+      
+      # actually call the function we are testing
+      @complete_robot.process_item(mock_workitem)
+    end
+    
+    it "should report an error if workflow update failed" do
+      mock_workitem = mock("complete_deposit_workitem")
+      mock_workitem.stub!(:druid).and_return("druid:jc837rq9922")
+      
+      Dor::WorkflowService.stub(:update_workflow_status).and_raise("Update workflow \"complete-deposit\" failed")
+      Dor::WorkflowService.should_receive(:update_workflow_status).with("dor", "druid:jc837rq9922", "googleScannedBookWF", "sdr-ingest-complete", "completed")
+      
+      lambda {@complete_robot.process_item(mock_workitem)}.should raise_error("Update workflow \"complete-deposit\" failed")
+    end
   end
 end
