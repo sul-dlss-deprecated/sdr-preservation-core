e0f50752d80cc665d0c652720bc477518367ebf3
ingest-dev <ingest-dev@sdr-ingest-dev.stanford.edu>
2010-07-07 09:09:11 -0700

    Added identityMetadata section in the test DOR object

--- lib/test_dor_object_creator.rb
+++ lib/test_dor_object_creator.rb
@@ -15,6 +15,7 @@ xml = <<-EOXML
 </workflow>
 EOXML
 
+
 if(ARGV.size != 1)
   puts 'You must provide a pid'
   exit
@@ -22,8 +23,27 @@ end
 
 pid = ARGV[0]
 
+identityMetadata = <<-EOXML
+  <identityMetadata objectId=#{pid}>
+  <objectType>agreement</objectType>
+  <objectId>druid:tx617qp8040</objectId>
+  <otherId name='uuid'>c2e47ead-9ab8-4da8-b447-a30f0e097ddd</otherId>
+  <sourceId source='sulair'>SULAIR_agreement_agreement</sourceId>
+  <citationTitle>Agreement object for agreements</citationTitle>
+  <citationCreator>DLSS staff</citationCreator>
+  <agreementId>druid:tx617qp8040</agreementId>
+  <tag>Agreements : Version 1</tag>
+  </identityMetadata>
+EOXML
+
 Fedora::Repository.register('http://fedoraAdmin:fedoraAdmin@dor-dev.stanford.edu/fedora')
 obj = ActiveFedora::Base.new(:pid => pid, :label => 'sdr robot testing')
+
+# Add the IdentityMetadataDS
+identityMetadataDS = ActiveFedora::Datastream.new(:pid=>PID, :dsid=>"identityMetadata", :dsLabel=>"identityMetadata", :blob=>identityMetadata)
+obj.add_datastream(identityMetadataDS)
+
+
 obj.save
 
 Dor::WorkflowService.create_workflow('dor', pid, 'googleScannedBookWF', xml)
