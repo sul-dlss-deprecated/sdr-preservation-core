69c5a0237c2d93db6a62fea5e975bc2122d2c7ef
Bess Sadler <bess@ibiblio.org>
2010-07-13 15:40:05 -0300

    Changed verify agreement so it can accept a druid on the command line. This makes it easier to test.

--- robots/sdrIngest/verify_agreement.rb
+++ robots/sdrIngest/verify_agreement.rb
@@ -19,32 +19,37 @@ module SdrIngest
     # the agreement_id of the current workitem
     attr_reader :agreement_id 
 
-    # Override the robot LyberCore::Robot.process_item method.
-    # - Finds the object's agreement object in DOR
-
+    # Extract the druid and pass it along to process_druid
+    # This allows the robot to accept either a work_item or a druid
     def process_item(work_item)
-
-      # Identifiers
       druid = work_item.druid
+      process_druid(druid)
+    end
+
+    # Finds the object's agreement object in DOR
+    def process_druid(druid)
+
       puts "Druid being processed is " + druid 
 
       # get the agreement id for this object
-
-      get_agreement_id(druid)
-
-      puts @agreement_id
+      @agreement_id ||= get_agreement_id(druid)
 
       # check if it is in sedora
       puts "SEDORA_URI is " + SEDORA_URI
-      LyberCore::Connection.get("http://fedoraAdmin:fedoraAdmin@sdr-fedora-dev.stanford.edu:80/fedora/objects/" + "#{@agreement_id}", {})
+      begin
+        LyberCore::Connection.get("http://fedoraAdmin:fedoraAdmin@sdr-fedora-dev.stanford.edu:80/fedora/objects/" + "#{@agreement_id}", {})
       #LyberCore::Connection.get(SEDORA_URI + "/objects/" + agreementId, {})
-
-      # If agreement object is not in Sedora then throw an exception
-      
+      rescue Net::HTTPServerException
+        # If agreement object is not in Sedora then throw an exception
+        raise "Couldn't find agreement object #{@agreement_id} in sedora"
+      rescue
+        raise "Something went wrong."
+      end
     end
 
+    # Given a druid, get its IDENTITY metadata datastream from Sedora and 
+    # extract the agreement_id
     def get_agreement_id(druid)
-
 
       # Declare resp outside of the http.start loop so it will be available after the loop ends
       resp = ""
@@ -60,7 +65,7 @@ module SdrIngest
       end
       doc = Nokogiri::XML(resp)
       puts doc.xpath("//agreementId/text()") 
-      @agreement_id = doc.xpath("//agreementId/text()")
+      doc.xpath("//agreementId/text()")
     end
   end
 end
@@ -68,5 +73,11 @@ end
 # This is the equivalent of a java main method
 if __FILE__ == $0
   dm_robot = SdrIngest::VerifyAgreement.new('sdrIngest', 'verify-agreement')
-  dm_robot.start
+  # If this robot is invoked with a specific druid, it will run for that druid only
+  if(ARGV[0])
+    puts "Verifying agreement for #{ARGV[0]}"
+    dm_robot.process_druid(ARGV[0])
+  else
+    dm_robot.start
+  end
 end
