6e43555d2369eb5b55b8a0fbcd9f21a0c8ed11e4
Alpana Pande <alpana@stanford.edu>
2010-07-27 15:49:46 -0700

    Moved to sdrIngest directory, and made some changes to conform to the new RegisterSDR script.

---
+++ spec/sdrIngest/register_sdr_spec.rb
@@ -0,0 +1,48 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'rubygems'
+require 'lyber_core'
+require 'sdrIngest/register_sdr'
+
+describe "connect to sedora" do
+
+  context "Registering SDR Object" do
+  it "should be able to connect to sedora" do
+
+    #@robot = GoogleScannedBook::RegisterSdr.new('googleScannedBook', 'register-sdr')
+    @robot = SdrIngest::RegisterSdr.new()
+    mock_workitem = mock("register_sdr_workitem")
+    druid = "sdrtwo:AlpanaTests" + "#{Process.pid}"
+    print druid
+    mock_workitem.stub!(:druid).and_return(druid)
+
+    Fedora::Repository.register(SEDORA_URI)
+
+
+    begin
+      obj = ActiveFedora::Base.new(:pid => mock_workitem.druid)
+      obj.save
+    rescue
+      $stderr.print $!
+    end
+
+  end
+
+  context "basic behaviour" do
+   it "can be created" do
+      x = SdrIngest::RegisterSdr.new()
+      x.should be_instance_of(SdrIngest::RegisterSdr)
+    end
+  end
+
+  it "should be able to connect to workflow service" do
+
+    
+
+  end
+
+  it "should be able to create a workflow" do
+
+  end
+
+end
+end
