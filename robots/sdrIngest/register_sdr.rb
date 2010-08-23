6c5e3d9697d4c26b10a8d3006cb90dbd7548c1c8
Alpana <alpana@stanford.edu>
2010-08-22 23:27:34 -0700

    Make sure only the druids that have not been registered run through this robot.

--- robots/sdrIngest/register_sdr.rb
+++ robots/sdrIngest/register_sdr.rb
@@ -39,17 +39,47 @@ module SdrIngest
          # Get the druid list
          # First, get_objects_for_workstep(repository, workflow, completed, waiting)
          
-         #puts "getting object list"
-         object_list_xml = DorService.get_objects_for_workstep("dor", "googleScannedBookWF", "sdr-ingest-transfer", "sdr-ingest-deposit")
-      
+         #puts "getting list of objects that have been transferred"
+         druids_already_transferred_list_xml = DorService.get_objects_for_workstep("dor", "googleScannedBookWF", "sdr-ingest-transfer", "sdr-ingest-deposit")
          # Then get the list of druids from the xml returned in the earlier step
-         #puts "getting druids"
-         @druids = DorService.get_druids_from_object_list(object_list_xml)
-      
-         # debugging
-         #puts @druids.length()
- 
-         # Process druids one by one
+         druids_already_transferred = DorService.get_druids_from_object_list(druids_already_transferred_list_xml)
+
+         # ^^^^^^^^^^^^^ debugging ^^^^^^^^^^^^^
+         #puts "\n \n DRUIDS already transferred length =   "
+	 #puts druids_already_transferred.length()
+	 #puts "\n ========  DRUIDS already transferred =========="
+ 	 #puts druids_already_transferred
+	 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+	 # +++++++++++++++++++++++
+	 # druids_already_transferred is the big list of all objects that are waiting for sdr-ingest-deposit to be completed. 
+	 # From this list filter out objects that have already been registered.	
+	 # The last sdr robot complete-deposit sets sdr-ingest-deposit's status to "complete"
+	 # +++++++++++++++++++++++
+	 druids_already_registered_xml = DorService.get_objects_for_workstep("sdr", "sdrIngestWF", "register-sdr", "complete-deposit" )
+	 druids_already_registered = DorService.get_druids_from_object_list(druids_already_registered_xml)
+
+
+         # ^^^^^^^^^^^^^ debugging ^^^^^^^^^^^^^
+         #puts "\n \n DRUIDS already registered  length =   "
+         #puts druids_already_registered.length()
+	 #puts " \n \n ========  DRUIDS already registered  =========="
+ 	 #puts druids_already_registered
+	 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+	 # +++++++++++++++++++++++
+	 # Now that we have the two lists, find objects in the transferred list that are not in the registered list. Because there
+	 # might be objects that have been registered in a previous run which have not yet made it all the way through to the 
+	 # complete-deposit robot which actually sets the status of sdr-ingest-deposit to "complete"
+	 # ( In Ruby, this is a simple "set difference", unlike most other languages : newlist = x - y )
+	 # +++++++++++++++++++++++
+	 @druids = druids_already_transferred - druids_already_registered
+
+         puts "About to process " + @druids.length.to_s  + " druids"
+	 puts " ###############"
+
+         # Now process druids one by one
          i = 0
         
          while i < @druids.length() do
@@ -118,6 +148,12 @@ module SdrIngest
           Dor::WorkflowService.create_workflow('sdr', druid, 'sdrIngestWF', workflow_xml) 
            
           puts "DONE : Create new workflow for #{druid}"
+
+	  # Dont do this. Creating a new workflow with "completed" status for register-sdr is enough
+	  # update register-sdr status to "completed" in sdrIngestWF
+	  #result = Dor::WorkflowService.update_workflow_status("sdr", druid, "sdrIngestWF", "register-sdr", "completed")
+	  #raise "Update workflow \"register-sdr\" failed." unless result
+
            
           return true
          
@@ -141,4 +177,3 @@ if __FILE__ == $0
   end
   puts "Register SDR done"   
 end
-
