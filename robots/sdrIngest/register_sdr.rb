7c9d12ca0ba86dd97d7aa4591d7c07f2a09c8ca3
Alpana Pande <alpana@stanford.edu>
2010-08-25 17:00:55 -0700

    Added more comments to print to stdout

--- robots/sdrIngest/register_sdr.rb
+++ robots/sdrIngest/register_sdr.rb
@@ -10,35 +10,36 @@ module SdrIngest
 
 # Creates +Sedora+ objects and bootstrapping the workflow.
 
-    class RegisterSdr 
-       
+    class RegisterSdr
+
        # Array of workitem objects to be processed
        attr_reader :druids
-       
+
        # The timings for the batch run
        attr_reader :start_time
        attr_reader :end_time
        attr :elapsed_time
-       
+
        # The tally of how many items have been processed
        attr_accessor :success_count
        attr_accessor :error_count
-       
+
        def initialize()
-  
+
          # Start the timer
          @start_time = Time.new
-         
+
          # Initialize the success and error counts
          @success_count = 0
          @error_count = 0
        end
-       
+
        def process_items()
-          
+
          # Get the druid list
          # First, get_objects_for_workstep(repository, workflow, completed, waiting)
-         
+
+         puts "\nGetting list of druids to process ... "
          #puts "getting list of objects that have been transferred"
          druids_already_transferred_list_xml = DorService.get_objects_for_workstep("dor", "googleScannedBookWF", "sdr-ingest-transfer", "sdr-ingest-deposit")
          # Then get the list of druids from the xml returned in the earlier step
@@ -46,48 +47,52 @@ module SdrIngest
 
          # ^^^^^^^^^^^^^ debugging ^^^^^^^^^^^^^
          #puts "\n \n DRUIDS already transferred length =   "
-	 #puts druids_already_transferred.length()
-	 #puts "\n ========  DRUIDS already transferred =========="
- 	 #puts druids_already_transferred
-	 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         #puts druids_already_transferred.length()
+         #puts "\n ========  DRUIDS already transferred =========="
+         #puts druids_already_transferred
+         # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-	 # +++++++++++++++++++++++
-	 # druids_already_transferred is the big list of all objects that are waiting for sdr-ingest-deposit to be completed. 
-	 # From this list filter out objects that have already been registered.	
-	 # The last sdr robot complete-deposit sets sdr-ingest-deposit's status to "complete"
-	 # +++++++++++++++++++++++
-	 druids_already_registered_xml = DorService.get_objects_for_workstep("sdr", "sdrIngestWF", "register-sdr", "complete-deposit" )
-	 druids_already_registered = DorService.get_druids_from_object_list(druids_already_registered_xml)
+        # +++++++++++++++++++++++
+         # druids_already_transferred is the big list of all objects that are waiting for sdr-ingest-deposit to be completed.
+         # From this list filter out objects that have already been registered.
+         # The last sdr robot complete-deposit sets sdr-ingest-deposit's status to "complete"
+         # +++++++++++++++++++++++
+         druids_already_registered_xml = DorService.get_objects_for_workstep("sdr", "sdrIngestWF", "register-sdr", "complete-deposit" )
+         druids_already_registered = DorService.get_druids_from_object_list(druids_already_registered_xml)
 
 
          # ^^^^^^^^^^^^^ debugging ^^^^^^^^^^^^^
          #puts "\n \n DRUIDS already registered  length =   "
          #puts druids_already_registered.length()
-	 #puts " \n \n ========  DRUIDS already registered  =========="
- 	 #puts druids_already_registered
-	 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         #puts " \n \n ========  DRUIDS already registered  =========="
+         #puts druids_already_registered
+         # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
-	 # +++++++++++++++++++++++
-	 # Now that we have the two lists, find objects in the transferred list that are not in the registered list. Because there
-	 # might be objects that have been registered in a previous run which have not yet made it all the way through to the 
-	 # complete-deposit robot which actually sets the status of sdr-ingest-deposit to "complete"
-	 # ( In Ruby, this is a simple "set difference", unlike most other languages : newlist = x - y )
-	 # +++++++++++++++++++++++
-	 @druids = druids_already_transferred - druids_already_registered
+         # +++++++++++++++++++++++
+         # Now that we have the two lists, find objects in the transferred list that are not in the registered list. Because there
+         # might be objects that have been registered in a previous run which have not yet made it all the way through to the
+         # complete-deposit robot which actually sets the status of sdr-ingest-deposit to "complete"
+         # ( In Ruby, this is a simple "set difference", unlike most other languages : newlist = x - y )
+         # +++++++++++++++++++++++
+         @druids = druids_already_transferred - druids_already_registered
 
+         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
          puts "About to process " + @druids.length.to_s  + " druids"
-	 puts " ###############"
+
+         #if (@druids.length() == 0)
+         #  puts "There are NO druids to process"
+         #end
 
          # Now process druids one by one
          i = 0
-        
+
          while i < @druids.length() do
            begin
              puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
              puts "Processing " + @druids[i]
              #puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-             if (process_druid(@druids[i]) == nil) 
+             if (process_druid(@druids[i]) == nil)
                @error_count += 1
                #puts "Errrrrrrrrrrroooooooooorrrrrrrrrrrr"
              else
@@ -96,40 +101,39 @@ module SdrIngest
              end
              i += 1
            end
-      
+
          end  # end while
          # Print success, error count
          print_stats
 
-       end 
-  
-  
+       end
+
        # Output the batch's timings and other statistics to STDOUT for capture in a log
        def print_stats
          @end_time = Time.new
          @elapsed_time = @end_time - @start_time
-         puts "\n \n"
+         puts "\n"
          puts "**********************************************"
          puts "Total time: " + @elapsed_time.to_s + "\n"
          puts "Completed objects: " + @success_count.to_s + "\n"
          puts "Errors: " + @error_count.to_s + "\n"
          puts "**********************************************"
        end
-  
-  
+
+
        # - Creates a *Sedora* object
        # - Initializes the +Deposit+ workflow
        def process_druid(druid)
-    
+
           begin
             Fedora::Repository.register(SEDORA_URI)
           rescue Errno::ECONNREFUSED => e
-            raise RuntimeError, "Can't connect to Fedora at url #{SEDORA_URI} : #{e}"  
+            raise RuntimeError, "Can't connect to Fedora at url #{SEDORA_URI} : #{e}"
             return nil
           end
           puts "DONE : Sedora registration"
-          
-         
+
+
           #puts "druid in sedora will be" + druid
           begin
             obj = ActiveFedora::Base.new(:pid => druid)
@@ -140,27 +144,27 @@ module SdrIngest
             puts "ERROR : Object cannot be saved in Sedora"
             return nil
           end
-             
+
           puts "DONE : Create new object in Sedora"
 
           # Initialize workflow
           workflow_xml = File.open(File.join(File.dirname(__FILE__), "..", "..", "config", "workflows", "sdrIngest", 'sdrIngestWorkflow.xml'), 'rb') { |f| f.read }
-          Dor::WorkflowService.create_workflow('sdr', druid, 'sdrIngestWF', workflow_xml) 
-           
+          Dor::WorkflowService.create_workflow('sdr', druid, 'sdrIngestWF', workflow_xml)
+
           puts "DONE : Create new workflow for #{druid}"
 
-	  # Dont do this. Creating a new workflow with "completed" status for register-sdr is enough
-	  # update register-sdr status to "completed" in sdrIngestWF
-	  #result = Dor::WorkflowService.update_workflow_status("sdr", druid, "sdrIngestWF", "register-sdr", "completed")
-	  #raise "Update workflow \"register-sdr\" failed." unless result
+          # Dont do this. Creating a new workflow with "completed" status for register-sdr is enough
+          # update register-sdr status to "completed" in sdrIngestWF
+          #result = Dor::WorkflowService.update_workflow_status("sdr", druid, "sdrIngestWF", "register-sdr", "completed")
+          #raise "Update workflow \"register-sdr\" failed." unless result
 
-           
+
           return true
-         
+
         end  # end process_item
-  
+
     end # end of class
-end # end of module 
+end # end of module
 
 
 # This is the equivalent of a java main method
@@ -169,11 +173,12 @@ if __FILE__ == $0
   if(ARGV[0])
     puts "Registering SDR with #{ARGV[0]}"
     sdr_bootstrap = SdrIngest::RegisterSdr.new()
-    sdr_bootstrap.process_druid(ARGV[0]) 
+    sdr_bootstrap.process_druid(ARGV[0])
     #sdr_bootstrap.print_stats()
   else
     sdr_bootstrap = SdrIngest::RegisterSdr.new()
-    sdr_bootstrap.process_items() 
+    sdr_bootstrap.process_items()
   end
-  puts "Register SDR done"   
+  puts "Register SDR done\n"
 end
+
