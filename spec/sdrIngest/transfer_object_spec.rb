52703a54a3c21584e1a5145f32e178de4c3d9588
Alpana Pande <alpana@stanford.edu>
2010-08-25 18:49:03 -0700

    Aadded require 'lyber_core/utils

--- spec/sdrIngest/transfer_object_spec.rb
+++ spec/sdrIngest/transfer_object_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'rubygems'
 require 'lyber_core'
+require 'lyber_core/utils'
 require 'sdrIngest/transfer_object'
 
 describe SdrIngest::TransferObject do
