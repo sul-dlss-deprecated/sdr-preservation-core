7c6a65e13d4ffd97b10c7a0a3405bc99ae57fa0d
Bess Sadler <bess@stanford.edu>
2011-05-19 14:41:37 -0700

    Making this spec more robust by ensuring that we refresh any fixture objects we use

--- spec/lib/datastream_remover_spec.rb
+++ spec/lib/datastream_remover_spec.rb
@@ -5,42 +5,32 @@ require 'active-fedora'
 require 'rest-client'
 
 require File.join(File.dirname(__FILE__), "..", "..", "lib", "datastream_remover.rb")
-ENABLE_SOLR_UPDATES = false
-
 
 describe DatastreamRemover do
   
   context "basic behavior" do
     
     def setup
-      @repository_url = "http://fedoraAdmin:fedoraAdmin@127.0.0.1:8983/fedora"
-      Fedora::Repository.register(@repository_url)
+      Fedora::Repository.register(SEDORA_URI)
       
-      @solr_url = "http://localhost:8983/solr/"
+      ActiveFedora::SolrService.register(SOLR_URL)
+      
       @fixture_pid = "fixture:contentmd_removal"
       @fixture_filename = File.join(File.dirname(__FILE__),"..","fixtures","#{@fixture_pid.gsub(":","_")}.foxml.xml")
-      
-      ActiveFedora::SolrService.register(@solr_url)
-      # 
-      # # make sure we're starting with a fresh object
+          
+      # make sure we're starting with a fresh object
       begin
-        obj = ActiveFedora::Base.load_instance(@fixture_pid)
-        RestClient.delete "#{@repository_url}/objects/#{@fixture_pid}"
+        ActiveFedora::Base.load_instance(@fixture_pid).delete
       rescue ActiveFedora::ObjectNotFoundError
-        # If the object wasn't found, that's fine, don't do anything.
-        puts "Object not found"
-      rescue
-        # But for any other kind of error, let us know what's wrong.
-        $stderr.print $!
       end
       
+      # test to ensure it was deleted
+      lambda{ActiveFedora::Base.load_instance(@fixture_pid)}.should raise_exception(ActiveFedora::ObjectNotFoundError)
+      
+      # now load the fixture 
       file = File.new(@fixture_filename, "r")
-      result = foxml = Fedora::Repository.instance.ingest(file.read)
-      if result
-        # puts "The fixture has been ingested as #{result}"  
-      else
-        raise "Failed to ingest the fixture."
-      end
+      result = foxml = Fedora::Repository.instance.ingest(file.read)      
+      raise "Failed to ingest the fixture." unless result
       
     end
 
@@ -58,7 +48,7 @@ describe DatastreamRemover do
     
     
     it "can instantiate" do
-      dr = DatastreamRemover.new(@repository_url)
+      dr = DatastreamRemover.new(SEDORA_URI)
       dr.class.should eql(DatastreamRemover)
     end
     
@@ -66,15 +56,15 @@ describe DatastreamRemover do
       datastream_name = "contentMetadata"
       batch_size = 1
       pid_namespace = 
-      dr = DatastreamRemover.new(@repository_url)
-      foo = RestClient.get "#{@repository_url}/objects/#{@fixture_pid}/datastreams/#{datastream_name}?versionable=false"
+      dr = DatastreamRemover.new(SEDORA_URI)
+      foo = RestClient.get "#{SEDORA_URI}/objects/#{@fixture_pid}/datastreams/#{datastream_name}?versionable=false"
       foo.code.should eql(200)
       dr.removeDatastream(datastream_name, batch_size, "fixture")
-      lambda { RestClient.get "#{@repository_url}/objects/#{@fixture_pid}/datastreams/#{datastream_name}?versionable=false" }.should raise_exception(/404/)      
+      lambda { RestClient.get "#{SEDORA_URI}/objects/#{@fixture_pid}/datastreams/#{datastream_name}?versionable=false" }.should raise_exception(/404/)      
     end
     
     it "fetches an initial batch of PIDs" do
-      dr = DatastreamRemover.new(@repository_url)
+      dr = DatastreamRemover.new(SEDORA_URI)
       fedora_query_fixture = File.join(File.dirname(__FILE__),"..","fixtures","fedora_query_results.xml")
       first_batch = dr.fetch_batch(1,"fixture")
       first_batch.length.should eql(1)
@@ -84,7 +74,7 @@ describe DatastreamRemover do
     it "extracts an array of pids from the fedora response" do
       fedora_query_fixture = File.join(File.dirname(__FILE__),"..","fixtures","fedora_query_results.xml")
       doc = Nokogiri::XML(File.open(fedora_query_fixture))
-      dr = DatastreamRemover.new(@repository_url)
+      dr = DatastreamRemover.new(SEDORA_URI)
       pid_array = dr.extract_pid_array(doc)
       pid_array[0].should eql("druid:mh502qk0176")
       pid_array.length.should eql(4)
@@ -93,7 +83,7 @@ describe DatastreamRemover do
     it "extracts a session token from the fedora response" do
       fedora_query_fixture = File.join(File.dirname(__FILE__),"..","fixtures","fedora_query_results.xml")
       doc = Nokogiri::XML(File.open(fedora_query_fixture))
-      dr = DatastreamRemover.new(@repository_url)
+      dr = DatastreamRemover.new(SEDORA_URI)
       token = dr.extract_session_token(doc)
       token.should eql("bfe62bf233befbff0b9952f44d97f938")
     end
