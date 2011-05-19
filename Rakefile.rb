34f5b35e649611b4f9f74c4d7b49ddd1a1b1042e
Bess Sadler <bess@stanford.edu>
2011-05-19 14:56:56 -0700

    Using jettywrapper instead of local TestServer class

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,8 +4,8 @@
 # 
 require 'rake'
 require 'rake/testtask'
-require 'hanna/rdoctask'
 require 'spec/rake/spectask'
+require 'jettywrapper'
 
 # Import external rake tasks
 Dir.glob('lib/tasks/*.rake').each { |r| import r }
@@ -25,44 +25,37 @@ task "test" do
   Rake::Task["examples"].invoke
 end
 
-desc "Do the whole build"
-task "hudson" do
-  Rake::Task["jetty"].invoke
- # Rake::Task["examples_with_rcov"].invoke # jetty already runs examples_with_rcov
-  Rake::Task["rdoc"].invoke
-end
-
 desc "Run RSpec examples"
 Spec::Rake::SpecTask.new('examples') do |t|
   t.spec_files = FileList['spec/**/*.rb']
 end
 
 desc "Run RSpec with RCov"
-Spec::Rake::SpecTask.new('examples_with_rcov') do |t|
+Spec::Rake::SpecTask.new('spec') do |t|
   t.spec_files = FileList['spec/**/*.rb']
   t.rcov = true
   t.rcov_opts = ['--exclude', 'spec,/usr/,/home/hudson']
 end
 
 desc "Run RSpec Examples wrapped in a test instance of jetty"
-task :jetty do
-  require File.expand_path(File.dirname(__FILE__) + '/spec/lib/test_jetty_server.rb')
-  
-  SOLR_PARAMS = {
-    :quiet => ENV['SOLR_CONSOLE'] ? false : true,
-    :jetty_home => ENV['SOLR_JETTY_HOME'] || File.expand_path('./hydra-jetty'),
-    :jetty_port => ENV['SOLR_JETTY_PORT'] || 8983,
-    :solr_home => ENV['SOLR_HOME'] || File.expand_path('./hydra-jetty/solr'),
-    :fedora_home => ENV['FEDORA_HOME'] || File.expand_path('./hydra-jetty/fedora'),
-    :startup_wait => 60
-  }
-  # wrap tests with a test-specific Solr server
-  error = TestJettyServer.wrap(SOLR_PARAMS) do
-    Rake::Task["examples_with_rcov"].invoke
-    # puts `ps aux | grep start.jar` 
+task :hudson do
+    if (ENV['RAILS_ENV'] == "test")
+      jetty_params = { 
+        :jetty_home => File.expand_path(File.dirname(__FILE__) + '/hydra-jetty'), 
+        :quiet => false, 
+        :jetty_port => 8983, 
+        :solr_home => File.expand_path(File.dirname(__FILE__) + '/hydra-jetty/solr'),
+        :fedora_home => File.expand_path(File.dirname(__FILE__) + '/hydra-jetty/fedora/default'),
+        :startup_wait => 30
+        }
+      error = Jettywrapper.wrap(jetty_params) do  
+        Rake::Task["spec"].invoke
+      end
+      raise "test failures: #{error}" if error
+    else
+      system("rake hudson RAILS_ENV=test")
+    end
   end
-  raise "test failures: #{error}" if error
-end
 
 desc "Generate HTML report for failing examples"
 Spec::Rake::SpecTask.new('failing_examples_with_html') do |t|
