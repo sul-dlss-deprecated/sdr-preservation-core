e3ca4c1d27d5821ec5d5e49a996c08241469c08e
Richard Anderson <rnanders@stanford.edu>
2011-06-28 13:46:15 -0600

    Deleted never-inmplemented features and cucumber rake task

--- features/support/workflow_helpers.rb
+++
@@ -1,9 +0,0 @@
-module WorkflowHelpers
-  
-  def nextid
-    d = Nokogiri::XML(open("http://dor-dev.stanford.edu/fedora/management/getNextPID?xml=true&namespace=sdrtwo", {:http_basic_authentication=>["fedoraAdmin", "fedoraAdmin"]}))
-    d.xpath("//pid").text
-  end
-  
-end
-World(WorkflowHelpers)
