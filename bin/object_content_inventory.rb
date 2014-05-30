65fe03ab4627f0c284512680383fa75a874604ad
Richard Anderson <rnanders@stanford.edu>
2014-05-30 17:52:05 -0600

    Add scripts for listing and object's files, or deleting objects from test environment

---
+++ bin/object_content_inventory.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'moab_stanford'
+include Stanford
+
+module SdrServices
+  Config = Confstruct::Configuration.new do
+      username  nil
+      password nil
+      storage_filesystems nil
+      rsync_destination_host nil
+      rsync_destination_home nil
+  end
+end
+
+environment = (
+  case `hostname -s`.chomp
+    when "sdr-services"
+      "sdr-services.rb"
+    when "sdr-services-test"
+      "sdr-services-test.rb"
+    else
+      'development'
+  end
+)
+require File.join(ENV['HOME'],"sdr-services-app/current/config/environments/#{environment}")
+
+
+#pull the file from the commandline argument and read each line into the druids array
+druids = []
+druidlist = File.open(ARGV[0])
+druidlist.each_line {|line|
+  druids.push line.chomp
+}
+
+#for each druid in the array, format it, retrieve the file information for the content group, pull out just the signature and file name, and finally out as a csv
+
+druids.each do |druid|
+  druid = "druid:#{druid}" unless druid.start_with?('druid')
+  content_group = StorageServices.retrieve_file_group('content',druid)
+  content_group.path_hash.each do |file,signature|
+    puts "#{druid}, #{file.to_s}, #{signature.md5}, #{signature.size}"
+  end
+end
---
+++ bin/purge_object.rb
@@ -0,0 +1,111 @@
+#!/usr/bin/env ruby
+#
+# Class and script to be used for deleting one or more objects from SDR Preservation Core
+# One can either pipe a list of druids to the script or specify a filename containing the list on the command line
+# syntax examples
+#
+# echo xg086nq7357 | ~/sdr-preservation-core/current/bin/bundle-exec.sh purge_object.rb
+# ~/sdr-preservation-core/current/bin/bundle-exec.sh purge_object.rb  myfile.txt
+
+# Figure out the deployment environment from the hostname
+environment = (
+case `hostname -s`.chomp
+  when "sdr-services"
+    'production'
+  when "sdr-services-test"
+    'staging'
+  when "sul-sdr-services-dev"
+    'integration'
+  else
+    'unknown'
+end
+)
+
+# Make sure this cannot run on the production machine
+case environment
+  when 'development', 'test', 'integration', 'staging'
+  else
+    puts "Not allowed to delete objects from #{environment} environment"
+    exit
+end
+
+require 'rubygems'
+require 'bundler/setup'
+require 'pathname'
+
+require 'moab_stanford'
+include Stanford
+module SdrServices
+  Config = Confstruct::Configuration.new do
+      username  nil
+      password nil
+      storage_filesystems nil
+      rsync_destination_host nil
+      rsync_destination_home nil
+  end
+end
+require File.join(ENV['HOME'], "sdr-services-app/current/config/environments/#{environment}")
+
+class PurgeObject
+
+  # Find the digital object's storage location
+  # @param druid [String] The object identifier
+  # @return [Moab::StorageObject] The storage object representing the digital object's storage
+  def find_storage_object(druid)
+    druid = "druid:#{druid}" unless druid.start_with?('druid')
+    StorageServices.find_storage_object(druid)
+  end
+
+  # Find the digital object version's storage sub-location (not used at present)
+  # @param druid [String] The object identifier
+  # @param [Integer] version_id the version identifier
+  # @return [Moab::StorageObjectVersion] The storage object version representing the digital object version's storage
+  def find_storage_object_version(druid, version_id=nil)
+    storage_object = find_storage_object(druid)
+    storage_object.find_object_version(version_id)
+  end
+
+  # Delete the specified object from preservation core
+  # @param druid [String] The object identifier
+  # @return [Boolean] true if delete succeeds, false on failure
+  def delete_object(druid)
+    storage_object = find_storage_object(druid)
+    object_pathname = storage_object.object_pathname
+    delete_storage(object_pathname)
+  end
+
+  # Delete the storage location of the specified object
+  # @param object_pathname [Pathname] The storage location of the object being deleted
+  # @return [Boolean] true if delete succeeds, false on failure
+  def delete_storage(object_pathname)
+    # retry up to 3 times
+    sleep_time = [0, 2, 6]
+    attempts ||= 0
+    object_pathname.rmtree if object_pathname.exist?
+    puts "deleted #{object_pathname}"
+    return true
+  rescue Exception => e
+    if (attempts += 1) < sleep_time.size
+      GC.start
+      sleep sleep_time[attempts].to_i
+      retry
+    else
+      puts "unable to delete #{object_pathname} (#{attempts} attempts)"
+      return false
+    end
+  end
+
+end
+
+# This is the equivalent of a java main method
+if __FILE__ == $0
+  po = PurgeObject.new
+  ARGF.each do |druid|
+    begin
+      druid.chomp!
+      po.delete_object(druid) unless druid.empty?
+    rescue
+    end
+
+  end
+end
