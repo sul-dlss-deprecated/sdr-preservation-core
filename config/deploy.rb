bdc0762a0624c464e8b4e468c79ffb270c09004b
Darren Weber <dlweber@stanford.edu>
2014-08-18 15:11:19 -0700

    Deploy fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,6 @@ set :deploy_to, "/var/#{fetch(:user)}/#{fetch(:application)}"
 
 # set the server variable
 server fetch(:hostname), user: fetch(:user), roles: %w{app}
-
 Capistrano::OneTimeKey.generate_one_time_key!
 
 # Default value for :scm is :git
