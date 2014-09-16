c3f6c5053ad92ff1a70787a0db4b1aca4d92fd71
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2014-09-16 16:38:40 -0700

    Config - enable a redis timeout option.

--- config/load_robots.rb
+++ config/load_robots.rb
@@ -9,7 +9,17 @@ require_relative 'workflows/sdrIngestWF/load_robots'
 # load the robot-controller into memory
 require 'resque'
 # REDIS_URL specified in config/environments/{environment}.rb file which was loaded in the boot script
-Resque.redis = REDIS_URL
+begin
+  if defined? REDIS_TIMEOUT
+    _server, _namespace = REDIS_URL.split('/', 2)
+    _host, _port, _db = _server.split(':')
+    _redis = Redis.new(:host => _host, :port => _port, :thread_safe => true, :db => _db, :timeout => REDIS_TIMEOUT.to_f)
+    Resque.redis = Redis::Namespace.new(_namespace, :redis => _redis)
+  else
+    Resque.redis = REDIS_URL
+  end
+end
+
 require 'active_support/core_ext' # camelcase
 require 'robot-controller'
 
