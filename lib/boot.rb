547962dc26c5ae579b402eea1de8b7c17c09197e
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-12-04 16:07:12 -0800

    update gems: dor-workflow-service, lyber-core, nokogiri, lyberteam-capistrano-devel, capistrano-bundle_audit, druid-tools, sdr-replication

--- lib/boot.rb
+++ lib/boot.rb
@@ -7,6 +7,21 @@ require_relative 'libdir'
 ROBOT_ROOT = Pathname(__dir__).parent.to_s
 HOME = ENV['HOME']
 
+# Sdr::Config contains the constants that are required within this project
+module Sdr
+  Config = Confstruct::Configuration.new do
+    ingest_transfer do
+      account "userid@dor-host.stanford.edu"
+      export_dir "/dor/export/"
+    end
+    logdir File.join(ROBOT_ROOT, 'log')
+    migration_source "/services-disk/sdr2objects"
+    sdr_recovery_home nil
+    enqueue_max 10
+    audit_verbose false
+  end
+end
+
 # The Dor::Config object is created in dor-services-gem/lib/dor/config.rb
 # and initialized with the data in dor-services-gem/config/config_defaults.yml
 module Dor
@@ -26,6 +41,14 @@ module Dor
    end
 end
 
+require 'dor/services/workflow_service'
+log_file = File.join(ROBOT_ROOT, 'log', 'workflow_service.log')
+wfs_logger = Logger.new(log_file, 'weekly')
+wfs_options = {
+  logger: wfs_logger
+}
+Dor::WorkflowService.configure Dor::Config.workflow.url, wfs_options
+
 #
 # The Moab::Config object is created in moab-versioning/lib/moab/config.rb
 # module Moab
@@ -37,24 +60,8 @@ end
 #   end
 # end
 
-# Sdr::Config contains the constants that are required within this project
-module Sdr
-  Config = Confstruct::Configuration.new do
-    ingest_transfer do
-      account "userid@dor-host.stanford.edu"
-      export_dir "/dor/export/"
-    end
-    logdir File.join(ROBOT_ROOT, 'log')
-    migration_source "/services-disk/sdr2objects"
-    sdr_recovery_home nil
-    enqueue_max 10
-    audit_verbose false
-  end
-end
-
 
 require 'druid-tools'
-require 'dor/services/workflow_service'
 require 'lyber_core/robot'
 require 'lyber_core/log'
 require 'moab_stanford'
@@ -66,32 +73,4 @@ environment = ENV['ROBOT_ENVIRONMENT'] || 'development'
 require File.join(ROBOT_ROOT,"config/environments/#{environment}")
 
 require 'sdr/sdr_robot'
-
-module Dor
-  module WorkflowService
-    class << self
-      # @param [String] url points to the workflow service
-      # @param [Hash] opts optional params
-      # @option opts [String] :client_cert_file path to an SSL client certificate
-      # @option opts [String] :client_key_file path to an SSL key file
-      # @option opts [String] :client_key_pass password for the key file
-      def configure(url, opts={})
-        params = {}
-        params[:timeout] = 120
-        params[:open_timeout] = 120
-        if opts[:client_cert_file]
-          cert_file = File.read(opts[:client_cert_file])
-          params[:ssl_client_cert] = OpenSSL::X509::Certificate.new(cert_file)
-        end
-        if opts[:client_key_file]
-          key_file = File.read(opts[:client_key_file])
-          params[:ssl_client_key]  = OpenSSL::PKey::RSA.new(key_file, opts[:client_key_pass])
-        end
-        @@resource = RestClient::Resource.new(url, params)
-      end
-    end
-  end
-end
-
-Dor::WorkflowService.configure Dor::Config.workflow.url
 
