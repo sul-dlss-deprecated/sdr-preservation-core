ee8150d5061c5d15e0725314cdd9a3ac8408a787
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2016-02-12 18:46:25 -0800

    Revise restart script

---
+++ bin/sdr-preservation-core_restart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# The ROBOT_ENVIRONMENT values here should match the
+# environment files in the shared configs at
+# https://github.com/sul-dlss/sdr-configs/tree/master/sdr-preservation-core/config/environments
+
+if hostname | egrep -q '\-dev'; then
+    export ROBOT_ENVIRONMENT='integration'
+fi
+if hostname | egrep -q '\-test|\-stage'; then
+    export ROBOT_ENVIRONMENT='test'
+fi
+if hostname | egrep -q '\-prod'; then
+    export ROBOT_ENVIRONMENT='production'
+fi
+
+cd ~/sdr-preservation-core/current
+bundle exec controller stop
+bundle exec controller quit
+bundle exec controller boot
+
